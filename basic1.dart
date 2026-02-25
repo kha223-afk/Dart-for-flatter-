2ad727e764fbf4d74c3e1968152bf655bbd91555
kha223-afk <mdkhalikulislam330@gmail.com>
2026-02-25 09:14:20 +0600

    first commit

---
+++ basic1.dart
@@ -0,0 +1,25 @@
+// void main() {
+//   String name = "md khalikul islam";
+//   print(name);
+// }
+void
+main() {
+  String name = "md khalikul islam";
+  // print(
+  //   name,
+  // );
+
+  List dept = [
+    12,
+    4,
+    5,
+    5,
+    6,
+    6,
+    7,
+    8,
+  ];
+  print(
+    dept,
+  );
+}
